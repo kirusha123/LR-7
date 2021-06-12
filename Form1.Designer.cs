be38d22ada17b9d54787e9cb39d222afbe3f1f74
kirusha123 <kirriess@mail.ru>
2021-06-12 16:40:38 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.RenderTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(-1, 0);
+            this.AnT.Margin = new System.Windows.Forms.Padding(2);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(762, 438);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            this.AnT.Load += new System.EventHandler(this.AnT_Load);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "вращать вдоль X",
+            "вращать вдоль Y",
+            "вращать вдоль Z"});
+            this.comboBox1.Location = new System.Drawing.Point(885, 66);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(824, 185);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar1.Maximum = 50000;
+            this.trackBar1.Minimum = -50000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 222);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(870, 185);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar2.Maximum = 50000;
+            this.trackBar2.Minimum = -50000;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 222);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(916, 185);
+            this.trackBar3.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar3.Maximum = 50000;
+            this.trackBar3.Minimum = -50000;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(45, 222);
+            this.trackBar3.TabIndex = 6;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(963, 185);
+            this.trackBar4.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar4.Maximum = 360;
+            this.trackBar4.Minimum = -360;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar4.Size = new System.Drawing.Size(45, 222);
+            this.trackBar4.TabIndex = 7;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(1010, 185);
+            this.trackBar5.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar5.Maximum = 5000;
+            this.trackBar5.Minimum = -5000;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar5.Size = new System.Drawing.Size(45, 222);
+            this.trackBar5.TabIndex = 8;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // RenderTimer
+            // 
+            this.RenderTimer.Enabled = true;
+            this.RenderTimer.Tick += new System.EventHandler(this.RenderTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(824, 167);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(870, 166);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(916, 166);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Z";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(824, 410);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(871, 410);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(917, 410);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(963, 167);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Angle";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1007, 166);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Zoom";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(883, 50);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "По оси";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 440);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.AnT);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.Timer RenderTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.FreeGlut;
+using Tao.OpenGl;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        //<---------------ABOBA------------------>
+
+        Bitmap bmp;
+        string dir = "./2.jpg";
+
+        Color col;
+
+        int height = 0;
+        int width = 0;
+
+        int n = 800;
+        int m = 945;
+
+        double[,] newM;
+        double[,] matr;
+
+        float[] color1 = new float[4] { 1, 0, 0, 1 };
+        float[] shininess = new float[1] { 30 };
+
+        //<--------------------------------->
+        public Form1()
+        {
+            InitializeComponent();
+            AnT.InitializeContexts();
+
+            //initMAN
+           
+        }
+
+        // вспомогательные переменные - в них будут храниться обработанные значения,
+        // полученные при перетаскивании ползунков пользователем
+        double a = 0, b = 0, c = -5, d = 0, zoom = 1; // выбранные оси
+
+        private void AnT_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int os_x = 1, os_y = 0, os_z = 0;
+        // режим сеточной визуализации
+       // bool Wire = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // инициализация библиотеки glut
+            Glut.glutInit();
+            // инициализация режима экрана
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE);
+            // установка цвета очистки экрана (RGBA)
+            Gl.glClearColor(255, 255, 255, 1);
+            // установка порта вывода
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+            // активация проекционной матрицы
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            // очистка матрицы
+            Gl.glLoadIdentity();
+            // установка перспективы
+            Glu.gluPerspective(45, (float)AnT.Width / (float)AnT.Height, 0.1, 200);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+            // начальная настройка параметров openGL (тест глубины, освещение и первый источник света)
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glEnable(Gl.GL_LIGHTING);
+            Gl.glEnable(Gl.GL_LIGHT0);
+            // установка первых элементов в списках combobox
+            comboBox1.SelectedIndex = 0;
+            // comboBox2.SelectedIndex = 0;
+            // активация таймера, вызывающего функцию для визуализации
+            image_to_bmp();
+            height = get_height(bmp);
+            width = get_width(bmp);
+
+
+            RenderTimer.Start();
+
+
+        }
+
+
+
+        private void image_to_bmp()
+        {
+            bmp = new Bitmap(dir);
+            matr = generate_matrix(bmp.Width, bmp.Height);
+
+        }
+
+        private int get_width(Bitmap b)
+        {
+            return b.Size.Width;
+        }
+
+        private int get_height(Bitmap b)
+        {
+            return b.Size.Height;
+        }
+        private double calc_L(int x, int y)
+        {
+            Color color = bmp.GetPixel(x, y);
+            return 1 / (1 + Math.Exp(-Math.Abs(color.GetHashCode() / 5000000)));
+        }
+
+        private int get_color(int x = 0, int y = 0)
+        {
+            col = bmp.GetPixel(x, y);
+
+            //pictureBox1.BackColor = col;
+            return col.GetHashCode();
+        }
+
+
+        private void RenderTimer_Tick(object sender, EventArgs e)
+        {
+            // вызываем функцию отрисовки сцены
+            Draw();
+        }
+        // событие изменения значения
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            // переводим значение, установившееся в элементе trackBar в необходимый нам формат
+            a = (double)trackBar1.Value / 1000.0;
+            // подписываем это значение в label элементе под данным ползунком
+            label4.Text = a.ToString();
+        }
+        // событие изменения значения
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            // переводим значение, установившееся в элементе trackBar в необходимый нам формат
+            b = (double)trackBar2.Value / 1000.0;
+            // подписываем это значение в label элементе под данным ползунком
+            label5.Text = b.ToString();
+        }
+        // событие изменения значения
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            // переводим значение, установившееся в элементе trackBar в необходимый нам формат
+            c = (double)trackBar3.Value / 1000.0;
+            // подписываем это значение в label элементе под данным ползунком
+            label6.Text = c.ToString();
+        }
+        // событие изменения значения
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            // переводим значение, установившееся в элементе trackBar в необходимый нам формат
+            d = (double)trackBar4.Value;
+            // подписываем это значение в label элементе под данным ползунком
+            //label6.Text = d.ToString();
+        }
+        // событие изменения значения
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            // переводим значение, установившееся в элементе trackBar в необходимый нам формат
+            zoom = (double)trackBar5.Value / 1000.0;
+            // подписываем это значение в label элементе под данным ползунком
+            //label6.Text = zoom.ToString();
+        }
+        // изменения значения чекбокса
+
+        // изменение в элементах comboBox
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // в зависимости от выбранного режима
+            switch (comboBox1.SelectedIndex)
+            {
+                // устанавливаем необходимую ось (будет использована в функции glRotate**)
+                case 0:
+                    {
+                        os_x = 1;
+                        os_y = 0;
+                        os_z = 0;
+                        break;
+                    }
+                case 1:
+                    {
+                        os_x = 0;
+                        os_y = 1;
+                        os_z = 0;
+                        break;
+                    }
+                case 2:
+                    {
+                        os_x = 0;
+                        os_y = 0;
+                        os_z = 1;
+                        break;
+                    }
+            }
+        }
+
+
+        bool f = true;
+
+        // функция отрисовки
+        private void Draw()
+        {
+            // очистка буфера цвета и буфера глубины
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glClearColor(255, 255, 255, 1);
+            // очищение текущей матрицы
+            Gl.glLoadIdentity();
+            // помещаем состояние матрицы в стек матриц, дальнейшие трансформации затронут только визуализацию объекта
+            Gl.glPushMatrix();
+            // производим перемещение в зависимости от значений, полученных при перемещении ползунков
+            Gl.glTranslated(a, b, c);
+            // поворот по установленной оси
+            Gl.glRotated(d, os_x, os_y, os_z);
+            // и масштабирование объекта
+            Gl.glScaled(zoom, zoom, zoom);
+            // в зависимости от установленного типа объекта
+            //Glut.glutSolidCube(0.20);
+
+            
+            // Glut.glutWireCube(2.8);
+
+            if (f)
+            {
+                if (n > width)
+                {
+                    n = width;
+                }
+
+                if (m > height)
+                {
+                    m = height;
+                }
+
+                newM = squeeze_matr(n, m, matr);
+                f = false;
+            }
+
+
+            PointF l_up = new PointF(-1.4f, 1.4f);
+            PointF r_down = new PointF(1.4f, -1.4f);
+
+
+            double size = (r_down.X - l_up.X) / n;
+            //MessageBox.Show(Convert.ToString(Convert.ToInt32(5 / 2)));
+            //double[,] matrix = new double[width,height];
+
+            bool first = true;
+
+
+
+            Gl.glTranslated(-size * n / 2, -size * m / 2, 0);
+            for (int i = 0; i < n; i++)
+            {
+                if (!first)
+                {
+                    Gl.glTranslated(size, 0, 0);
+                }
+                else
+                {
+                    first = false;
+                }
+
+                for (int j = 0; j < m; j++)
+                {
+
+                    color1 = new float[4] {(float)bmp.GetPixel(n-1-i, m-1-j).R/255, (float)bmp.GetPixel(n-1-i, m-1-j).R/255, (float)bmp.GetPixel(n-1-i, m-1-j).R/255, 1};
+                    //(bmp.GetPixel(i-1,j-1).R, bmp.GetPixel(i - 1, j - 1).G, bmp.GetPixel(i - 1, j - 1).B);
+                    Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, color1);
+                    Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_SPECULAR, color1);
+                    Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_SHININESS, shininess);
+
+                    Glut.glutSolidCylinder(size / 2, newM[i,j], 15, 15);
+                    Gl.glTranslated(0,size,0);
+                }
+                Gl.glTranslated(0, -size * m, 0);
+            }/**/
+
+
+            // возвращаем состояние матрицы
+            Gl.glPopMatrix();
+            // завершаем рисование
+            Gl.glFlush();
+            // обновляем элемент AnT 
+            AnT.Invalidate();
+            //MessageBox.Show(Convert.ToString(size)+" "+Convert.ToString(newM[0,0]));
+            //RenderTimer.Stop();
+
+        }
+
+        private double[,] generate_matrix(int width, int height)
+        {
+            double[,] matrix = new double[width, height];
+            for (int i = width; i > 0; i--)
+            {
+                for (int j = height; j > 0; j--)
+                {
+                    matrix[width - i, height - j] = calc_L(i - 1, j - 1);
+                }
+
+            }
+
+            return matrix;
+        }
+
+        private double[,] squeeze_matr(int n, int m, double[,]  old)
+        {
+            
+            int coefX = Convert.ToInt32(width/n);
+            int coefY = Convert.ToInt32(height/m);
+            if  ((coefX > 1) && (coefY > 1)) { 
+                double[,] new_matr = new double[n,m];
+                int halfX = coefX/2;
+                int halfY = coefY/2;
+                for (int i = 0; i < n; i += 1)
+                {
+                    for (int j = 0; j < m; j += 1)
+                    {
+                        new_matr[i, j] = get_midle_length(i*coefX, j*coefX, halfX, halfY, width, height, old);
+                    }
+                }
+                //MessageBox.Show("Ready");
+                return new_matr;
+            }
+           
+            return old;
+        }
+
+        private double get_midle_length(int x, int y, int hx, int hy, int xmax, int ymax, double[,] old) 
+        {
+            double sum = 0;
+            int count = 0;
+            for (int i = x-hx; i < x+hx; i++)
+            {
+                for (int j = y-hy; j < y+hy; j++)
+                {
+                    if (((i > 0) && (i < xmax)) && ((j > 0) && (j < ymax)))
+                    {
+                        count++;
+                        sum += old[i, j];
+                    }
+                }
+            }
+            //MessageBox.Show(Convert.ToString(sum / count));
+            //return sum / count;
+            return (sum/count)/(Math.Sqrt(1+Convert.ToDouble(sum/count)));
+        }
+    }
+
+    // обрабатываем отклик таймера
+
+}
