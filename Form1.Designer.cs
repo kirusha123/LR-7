9c850b2f68dc4894bc4254ae359bb2612ca8b741
Volkov190 <f.volkov10@gmail.com>
2021-06-14 16:55:59 +0300

    full upgade old interface

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,11 +46,19 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.numericUp_n = new System.Windows.Forms.NumericUpDown();
+            this.numericUp_m = new System.Windows.Forms.NumericUpDown();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUp_n)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUp_m)).BeginInit();
             this.SuspendLayout();
             // 
             // AnT
@@ -64,9 +72,9 @@
             this.AnT.ColorBits = ((byte)(32));
             this.AnT.DepthBits = ((byte)(16));
             this.AnT.Location = new System.Drawing.Point(-1, 0);
-            this.AnT.Margin = new System.Windows.Forms.Padding(2);
+            this.AnT.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.AnT.Name = "AnT";
-            this.AnT.Size = new System.Drawing.Size(762, 438);
+            this.AnT.Size = new System.Drawing.Size(1016, 539);
             this.AnT.StencilBits = ((byte)(0));
             this.AnT.TabIndex = 0;
             this.AnT.Load += new System.EventHandler(this.AnT_Load);
@@ -78,70 +86,70 @@
             "вращать вдоль X",
             "вращать вдоль Y",
             "вращать вдоль Z"});
-            this.comboBox1.Location = new System.Drawing.Point(885, 66);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Location = new System.Drawing.Point(1180, 81);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(824, 185);
-            this.trackBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar1.Location = new System.Drawing.Point(1099, 228);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.trackBar1.Maximum = 50000;
             this.trackBar1.Minimum = -50000;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar1.Size = new System.Drawing.Size(45, 222);
+            this.trackBar1.Size = new System.Drawing.Size(56, 273);
             this.trackBar1.TabIndex = 4;
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // trackBar2
             // 
-            this.trackBar2.Location = new System.Drawing.Point(870, 185);
-            this.trackBar2.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar2.Location = new System.Drawing.Point(1160, 228);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.trackBar2.Maximum = 50000;
             this.trackBar2.Minimum = -50000;
             this.trackBar2.Name = "trackBar2";
             this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar2.Size = new System.Drawing.Size(45, 222);
+            this.trackBar2.Size = new System.Drawing.Size(56, 273);
             this.trackBar2.TabIndex = 5;
             this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
             // 
             // trackBar3
             // 
-            this.trackBar3.Location = new System.Drawing.Point(916, 185);
-            this.trackBar3.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar3.Location = new System.Drawing.Point(1221, 228);
+            this.trackBar3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.trackBar3.Maximum = 50000;
             this.trackBar3.Minimum = -50000;
             this.trackBar3.Name = "trackBar3";
             this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar3.Size = new System.Drawing.Size(45, 222);
+            this.trackBar3.Size = new System.Drawing.Size(56, 273);
             this.trackBar3.TabIndex = 6;
             this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
             // 
             // trackBar4
             // 
-            this.trackBar4.Location = new System.Drawing.Point(963, 185);
-            this.trackBar4.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar4.Location = new System.Drawing.Point(1284, 228);
+            this.trackBar4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.trackBar4.Maximum = 360;
             this.trackBar4.Minimum = -360;
             this.trackBar4.Name = "trackBar4";
             this.trackBar4.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar4.Size = new System.Drawing.Size(45, 222);
+            this.trackBar4.Size = new System.Drawing.Size(56, 273);
             this.trackBar4.TabIndex = 7;
             this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
             // 
             // trackBar5
             // 
-            this.trackBar5.Location = new System.Drawing.Point(1010, 185);
-            this.trackBar5.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar5.Location = new System.Drawing.Point(1347, 228);
+            this.trackBar5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.trackBar5.Maximum = 5000;
             this.trackBar5.Minimum = -5000;
             this.trackBar5.Name = "trackBar5";
             this.trackBar5.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar5.Size = new System.Drawing.Size(45, 222);
+            this.trackBar5.Size = new System.Drawing.Size(56, 273);
             this.trackBar5.TabIndex = 8;
             this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
             // 
@@ -153,98 +161,142 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(824, 167);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(1099, 206);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.Size = new System.Drawing.Size(17, 17);
             this.label1.TabIndex = 9;
             this.label1.Text = "X";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(870, 166);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(1160, 204);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.Size = new System.Drawing.Size(17, 17);
             this.label2.TabIndex = 10;
             this.label2.Text = "Y";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(916, 166);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(1221, 204);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.Size = new System.Drawing.Size(17, 17);
             this.label3.TabIndex = 11;
             this.label3.Text = "Z";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(824, 410);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(1099, 505);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.Size = new System.Drawing.Size(16, 17);
             this.label4.TabIndex = 12;
             this.label4.Text = "0";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(871, 410);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(1161, 505);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.Size = new System.Drawing.Size(16, 17);
             this.label5.TabIndex = 13;
             this.label5.Text = "0";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(917, 410);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(1223, 505);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.Size = new System.Drawing.Size(16, 17);
             this.label6.TabIndex = 14;
             this.label6.Text = "0";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(963, 167);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Location = new System.Drawing.Point(1284, 206);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.Size = new System.Drawing.Size(44, 17);
             this.label7.TabIndex = 15;
             this.label7.Text = "Angle";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(1007, 166);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Location = new System.Drawing.Point(1343, 204);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.Size = new System.Drawing.Size(44, 17);
             this.label8.TabIndex = 16;
             this.label8.Text = "Zoom";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(883, 50);
-            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Location = new System.Drawing.Point(1177, 62);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(42, 13);
+            this.label9.Size = new System.Drawing.Size(53, 17);
             this.label9.TabIndex = 17;
             this.label9.Text = "По оси";
             // 
+            // numericUp_n
+            // 
+            this.numericUp_n.Location = new System.Drawing.Point(1096, 119);
+            this.numericUp_n.Name = "numericUp_n";
+            this.numericUp_n.Size = new System.Drawing.Size(120, 22);
+            this.numericUp_n.TabIndex = 18;
+            this.numericUp_n.ValueChanged += new System.EventHandler(this.numericUp_n_ValueChanged);
+            // 
+            // numericUp_m
+            // 
+            this.numericUp_m.Location = new System.Drawing.Point(1096, 147);
+            this.numericUp_m.Name = "numericUp_m";
+            this.numericUp_m.Size = new System.Drawing.Size(120, 22);
+            this.numericUp_m.TabIndex = 19;
+            this.numericUp_m.ValueChanged += new System.EventHandler(this.numericUp_m_ValueChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(1047, 121);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(18, 17);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "N";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(1047, 152);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(19, 17);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "M";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1312, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 38);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "open image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1091, 440);
+            this.ClientSize = new System.Drawing.Size(1455, 542);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.numericUp_m);
+            this.Controls.Add(this.numericUp_n);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -261,7 +313,7 @@
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.AnT);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -270,6 +322,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUp_n)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUp_m)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -294,6 +348,12 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUp_n;
+        private System.Windows.Forms.NumericUpDown numericUp_m;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
